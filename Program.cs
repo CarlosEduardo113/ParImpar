34e5e8b462886775d52424ec7564504b924ba9d5
CarlosEduardo113 <ce219560@gmail.com>
2025-05-17 16:28:28 -0300

    Finalizar Programa

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿  Console.Write("Digite um número, agora!: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"{numero} é par");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} é ímpar");
+        }
